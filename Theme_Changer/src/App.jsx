83e3d14a91431b1535dc78df8fa139cbc13daadc
Vikash0216 <vikashmahatoravi@gmail.com>
2025-02-09 00:28:22 +0530

    Card  an button added

--- Theme_Changer/src/App.jsx
+++ Theme_Changer/src/App.jsx
@@ -5,9 +5,19 @@ function App() {
 
 
   return (
-    <div>
-      <h1 className='bg-sky-200'>Hello</h1>
+    
+<div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Themebtn */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+        </div>
     </div>
+</div>
+
   )
 }
 
