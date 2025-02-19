168ded10cd8a80b19797b448e29f6264f17509e1
Vikash0216 <vikashmahatoravi@gmail.com>
2025-02-19 18:57:22 +0530

    Store Created

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,11 +1,12 @@
+import { useState } from 'react'
 import './App.css'
-import config from './configVar/config';
 
 function App() {
   // console.log(process.env._REACT_APP_APPWRITE_URL)
   // We cannot do this. Every enviorment like vite or next can be accessed diffrently 
   // console.log(import.meta.env.VITE_APPWRITE_URL)
-  console.log(config.appwriteDatabaseId);
+  // console.log(config.appwriteDatabaseId);
+  const [loading ,setLoading] = useState()
   
 
   return (
---
+++ MegaProject/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer =()=>{
+    return(
+        <div>
+            <h2>I m header</h2>
+        </div>
+    )
+}
+export default Footer
